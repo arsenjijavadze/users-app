1d7099d620d50289053c75418ea72e17cfd56558
Arsen Jijavadze <arsen.jijavadze@gmail.com>
2023-01-04 01:23:49 +0300

    feat: implement information block and button in the success component

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -4,7 +4,9 @@ export const Success = ({ count }) => {
     return (
         <div className="success-block">
             <img src="/assets/success.svg" als="Success" />
-
+            <h3>Success!</h3>
+            <p>An invitation has been sent to all {count} users.</p>
+            <button className="send-invite-btn">Back</button>
         </div>
-    )
+    );
 };
