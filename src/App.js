a19da2adc92e5ca2ee24f1c6b24e6d269227ab09
Arsen Jijavadze <arsen.jijavadze@gmail.com>
2023-01-04 01:14:12 +0300

    feat: add basic page layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import './App.css';
+import './index.scss';
+import { Success } from './components/Success';
+import { Users } from './components/Users';
+
+//* users list https://reqres.in/api/users
 
 function App() {
   return (
     <div className="App">
-
+      <Success />
     </div>
   );
 }
---
+++ src/components/Success.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Success = ({ count }) => {
+    return (
+        <div className="success-block">
+            <img src="/assets/success.svg" als="Success" />
+
+        </div>
+    )
+};
