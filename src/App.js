e66e0310f95dc8e9267ba0732c48b24adb1ff466
Arsen Jijavadze <arsen.jijavadze@gmail.com>
2023-01-05 00:35:47 +0300

    feat: implement users list and search box

--- src/App.js
+++ src/App.js
@@ -6,9 +6,30 @@ import { Users } from './components/Users';
 //* users list https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = React.useState([]);
+  const [isLoading, setLoading] = React.useState(true);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  React.useEffect(() => {
+    fetch('https://reqres.in/api/users').then(res => res.json()).then(json => {
+      setUsers(json.data);
+    }).catch(err => {
+      console.warn(err);
+      alert('Error in getting data')
+    }).finally(() => setLoading(false));
+  }, []);
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  }
+
   return (
     <div className="App">
-      <Users />
+      <Users
+        onChangeSearchValue={onChangeSearchValue}
+        searchValue={searchValue}
+        items={users}
+        isLoading={isLoading} />
       {/* <Success /> */}
 
     </div>
