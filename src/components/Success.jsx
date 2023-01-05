512afd57228fca62c26a5b2196530960cca362bd
Arsen Jijavadze <arsen.jijavadze@gmail.com>
2023-01-05 12:55:57 +0300

    fix: implement correct loading data for success plus and minus symbols

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Success = ({ count }) => {
     return (
         <div className="success-block">
-            <img src="/assets/success.svg" als="Success" />
+            <img src="./assets/success.svg" als="Success" />
             <h3>Success!</h3>
             <p>An invitation has been sent to {count} users.</p>
             <button onClick={() => window.location.reload()} className="send-invite-btn">Back</button>
